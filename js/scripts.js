f9297bde6d1a658fc0f5924cb0c25f14325da525
ow <volovykolya@gmail.com>
2019-02-02 17:08:09 -0800

    add objects and some functions for the oblects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,43 @@
 // Back end ___________________________
+function Pizza(size, toppings){
+  this.sizing = [],
+  this.toppings = []
+}
+
+Pizza.prototype.addTopping = function(topping){
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.addSize = function(size){
+  this.sizing.push(size);
+}
+
+Pizza.prototype.totalCost = function(){
+  toppingsTotal = 0;
+  this.toppings.forEach(function(topping){
+    toppingsTotal += topping.price;
+  });
+  return toppingsTotal + size.price;
+}
+
+function Size(name, price){
+  this.name = name,
+  this.price = price
+}
+
+function Topping(name, price){
+  this.name = name,
+  this.price = price
+}
 
 
+
+var toppingCheese = new Topping ('cheese', 2);
+var toppingBacon = new Topping ('bacon', 3);
+var size = new Size("small", 2)
+var pizza1 = new Pizza ();
+pizza1.addTopping(toppingBacon);
+pizza1.addTopping(toppingCheese);
 
 
 
